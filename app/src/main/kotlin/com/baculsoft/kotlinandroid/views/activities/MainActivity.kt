3695f7e601a984a1ec31ab62b3326ab7317208c8
nanoteshivam <142962143+nanoteshivam@users.noreply.github.com>
2024-10-13 21:32:11 +0530

    Update MainActivity.kt

--- app/src/main/kotlin/com/baculsoft/kotlinandroid/views/activities/MainActivity.kt
+++ app/src/main/kotlin/com/baculsoft/kotlinandroid/views/activities/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setToolbar()
+        setToolbar()
         setButton()
     }
 
